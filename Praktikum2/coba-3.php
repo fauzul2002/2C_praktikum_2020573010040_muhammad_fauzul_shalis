60c5cb45ce1ed90ac81e8178f0490afaca21ff5a
fauzul2002 <fauzulshalis5@gmail.com>
2021-09-20 10:04:12 +0700

    pemakaian variabel 2

---
+++ Praktikum2/coba-3.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<html>
+    <body>
+        <?php
+            $jalan = "Medan - B.Aceh";
+            $norumah = 29;
+            $blok = "4c";
+            $jumlahpenghuni = 3;
+            echo "hasil variabel variabel + variabel norumah adalah ";
+            echo $alamat = $jalan. $norumah;
+            echo "<br>";
+            echo "hasil variabel variabel norumah + variabel jumlahpenghuni adalah";
+            echo $hasil = &$norumah + $jumlahpenghuni;
+            echo "<br>";
+            echo "hasil variabel variabel blok + variabel norumah adalah";
+                echo $hasil = $blok + $norumah;
+            echo "<br>";
+        ?>
+    </body>
+</html>
+</body>
+</html>
