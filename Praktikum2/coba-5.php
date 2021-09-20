cc7bd483a9a3f1c1acc42cdd728f8d6c8b85aacc
fauzul2002 <fauzulshalis5@gmail.com>
2021-09-20 10:19:26 +0700

    operasi  matematika

---
+++ Praktikum2/coba-5.php
@@ -0,0 +1,17 @@
+<html>
+    <body>
+        <?php
+            $jumlahbarang = 3;
+            $harga = 100; echo $pepmbayaran = $jumlahbarang + $harga;
+            echo "<br>";
+            echo $pembayaran = $jumlahbarang - $harga;
+            echo "<br>";
+            echo $pembayaran = $jumlahbarang * $harga;
+            echo "<br>";
+            echo $pembayaran = $jumlahbarang / $harga;
+            echo "<br>";
+            echo $pembayaran = $jumlahbarang % $harga;
+            echo "<br>";
+        ?>
+    </body>
+</html>
