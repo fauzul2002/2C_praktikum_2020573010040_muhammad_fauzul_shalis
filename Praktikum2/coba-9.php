c006b4dda3493aefc25d42188a23d090e7c1ccc1
fauzul2002 <fauzulshalis5@gmail.com>
2021-09-20 11:53:38 +0700

    operator perbandingan 1

--- Praktikum2/coba-9.php
+++ Praktikum2/coba-9.php
@@ -8,56 +8,54 @@
 </head>
 <body>
     <?php
-    $val1 = 0 ;
-    $val2 = 0 ;
-    echo "Operasi $val1 and $val2 adalah =";
-    echo $val1 && $val2 ;
-    echo "<br>" ;
-    echo "Operasi $val1 or $val2 adalah =";
-    echo $val1 || $val2 ;
-    echo "<br>" ;
-    echo "Operasi $val1 Xor $val2 adalah =";
-    echo $val1 Xor $val2 ;
-    echo "<br>" ;
-    echo "<br>" ;
+    $x= 4;
+    $a = ($x == 4);
+    echo "$a = $a <br>";
+    $b = ($x === 4);
+    echo "$b = $b <br>";
+    $c = ($x != 4);
+    echo "$c = $c <br>";
+    $d = ($x !== 4);
+    echo "$d = $d <br>";
+    $e = ($x < 5);
+    echo "$e = $e <br>";
+    $f = ($x > 5);
+    echo "$f = $f <br>";
+    $g = ($x <= 4);
+    echo "$g = $g <br>";
+    $h = ($x >= 5);
+    echo "$h = $h <br>";
 
-    $val1 = 0 ;
-    $val2 = 1 ;
-    echo "Operasi $val1 and $val2 adalah =";
-    echo $val1 && $val2 ;
-    echo "<br>" ;
-    echo "Operasi $val1 or $val2 adalah =";
-    echo $val1 || $val2 ;
-    echo "<br>" ;
-    echo "Operasi $val1 Xor $val2 adalah =";
-    echo $val1 Xor $val2 ;
-    echo "<br>" ;
-    echo "<br>" ;
+    ?>
+</body>
+</html><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $x= 4;
+    $a = ($x == 4);
+    echo "$a = $a <br>";
+    $b = ($x === 4);
+    echo "$b = $b <br>";
+    $c = ($x != 4);
+    echo "$c = $c <br>";
+    $d = ($x !== 4);
+    echo "$d = $d <br>";
+    $e = ($x < 5);
+    echo "$e = $e <br>";
+    $f = ($x > 5);
+    echo "$f = $f <br>";
+    $g = ($x <= 4);
+    echo "$g = $g <br>";
+    $h = ($x >= 5);
+    echo "$h = $h <br>";
 
-    $val1 = 1;
-    $val2 = 0;
-    echo "Operasi $val1 and $val2 adalah =";
-    echo $val1 && $val2 ;
-    echo "<br>" ;
-    echo "Operasi $val1 or $val2 adalah =";
-    echo $val1 || $val2 ;
-    echo "<br>" ;
-    echo "Operasi $val1 Xor $val2 adalah =";
-    echo $val1 Xor $val2 ;
-    echo "<br>" ;
-    echo "<br>" ;
-
-    $val1 = 1;
-    $val2 = 1;
-    echo "Operasi $val1 and $val2 adalah =";
-    echo $val1 && $val2 ;
-    echo "<br>" ;
-    echo "Operasi $val1 or $val2 adalah =";
-    echo $val1 || $val2 ;
-    echo "<br>" ;
-    echo "Operasi $val1 Xor $val2 adalah =";
-    echo $val1 Xor $val2 ;
-    echo "<br>" ;
     ?>
 </body>
 </html>
