4c0d5ad0b916372c9d0d12c67bc39284a36152a7
fauzul2002 <fauzulshalis5@gmail.com>
2021-09-26 19:48:22 +0700

    percabangan if else

--- praktikum 3/3.php
+++ praktikum 3/3.php
@@ -14,16 +14,19 @@
             echo "<br>"; // ganti baris
             $jumlah = 0;
             $kata = "belajar PHP";// suatu kata 
-            $huruf = "a"; // huruf vokal yang dicari bisa huruf a,e,i,u,o for ($i=0; $i<strlen($kata); $i++)
+            $huruf = "a"; // huruf vokal yang dicari bisa huruf a,e,i,u,o for ($i=0; $i<strlen($kata) ; $i++)
             {
                 if (substr($kata, $i , 1)== $huruf)
                 {
-                    $jumlah ++
+                $jumlah ++ ;
+
                 }
             }
-            echo "jumlah huruf ". $huruf . "dalam kata " .$kata . ":";
-echo "<br>"; echo $jumlah;  ?>
+            echo "jumlah huruf ". $huruf . "dalam kata " .$kata . " : ";
+echo "<br>"; echo $jumlah;   ?>
         </body>
     </html>
-</body>
+        </body>
 </html>
+
+
---
+++ praktikum 3/7.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <html>
+        <body>
+            <?php
+            $d=date("D");if ($d=="sat")
+            echo "selamat berakhir pekan";
+            elseif ($d=="fri")
+            echo "selamat menunaikan sholat jumat bagi kaum muslim !"; else
+            echo "selamat belajar!";
+?>
+        </body>
+    </html>
+</body>
+</html>
