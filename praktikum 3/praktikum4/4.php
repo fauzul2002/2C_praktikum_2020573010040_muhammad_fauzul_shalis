cdbd1ad8eaf30c89c585b873ec740317ea80a206
fauzul2002 <fauzulshalis5@gmail.com>
2021-09-29 23:03:57 +0700

    prak4 4.php

---
+++ praktikum 3/praktikum4/4.php
@@ -0,0 +1,12 @@
+<?php 
+$telpon ["farid"]= "08632761";
+$telpon ["fikri"]= "08656761";
+$telpon ["nida"]= "0863111";
+echo "Telpon farid :".$telpon ['farid'];
+echo "<br>";
+echo "Telpon fikri :".$telpon ['fikri'];
+echo "<br>";
+echo "Telpon nida:".$telpon ['nida'];
+echo "<br>";
+
+?>
