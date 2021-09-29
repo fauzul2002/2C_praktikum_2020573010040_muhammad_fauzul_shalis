4af5f2e95d7638d7356ce0a20b517352bd9f3407
fauzul2002 <fauzulshalis5@gmail.com>
2021-09-29 23:48:36 +0700

    prak4 7.php

---
+++ praktikum 3/praktikum4/7.php
@@ -0,0 +1,8 @@
+<?php
+$str = "belajar php ternyata menyenangkan";
+echo strtolower($str); //Ubah huruf ke kecil semua
+echo "<br>";
+echo strtoupper($str); //Ubah huruf ke besar semua 
+echo "<br>";
+echo str_replace("menyenangkan","mudah lho",$str);//mengganti string
+?>
