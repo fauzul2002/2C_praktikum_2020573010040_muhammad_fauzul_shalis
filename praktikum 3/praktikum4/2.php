16ba0f99af106d91a044c86316d71c9f35ea26a5
fauzul2002 <fauzulshalis5@gmail.com>
2021-09-29 22:35:40 +0700

    prak4 2.php

---
+++ praktikum 3/praktikum4/2.php
@@ -0,0 +1,12 @@
+<?php 
+$anak [] = "fauzul";
+$anak [] = "shalis";
+$anak [] = "faruq" ; echo
+"hallo";
+echo "isi array \$anak [0] adalah $nama[0]";echo
+"<br>";
+echo "isi array \$anak [1] adalah $nama[1]";echo
+"<br>";
+echo "isi array \$anak [2] adalah $nama[2]";echo
+"<br>";
+?>
