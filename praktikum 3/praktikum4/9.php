110af8168d730c27870cdb932231577b6f87b1f7
fauzul2002 <fauzulshalis5@gmail.com>
2021-10-01 22:17:19 +0700

    prak 4 9.php

---
+++ praktikum 3/praktikum4/9.php
@@ -0,0 +1,10 @@
+<?php 
+function garis ()
+{
+    echo "<hr>";
+}
+echo "ini contoh fungsi yang tanpa parameter <br>";
+garis();
+echo "lihat perbedaan dengan fungsi yang dengan parameter <br>";
+garis();
+?>
