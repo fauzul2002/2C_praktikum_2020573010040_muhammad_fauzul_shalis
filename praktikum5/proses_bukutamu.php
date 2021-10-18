0024d2db954102a970087d1081d9e821fdef38e1
fauzul2002 <fauzulshalis5@gmail.com>
2021-10-18 12:34:07 +0700

    bukutamu

--- praktikum5/proses_bukutamu.php
+++ praktikum5/proses_bukutamu.php
@@ -4,21 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Contoh form dengan post</title>
 </head>
 <body>
     <h1>Buku Tamu</h1>
-    komentar dan saran sangat kami butuhkan untuk meningkatkan kualitas situs kami.
+    komentar sangat di butuhkan dalam situs kami, untuk meningkatkan kualitas situs kami.
     <hr>
-    <form action="proses_bukutamu.php" method="POST">
+    <form action="proses_bukutamu.php" method="post">
         <pre>
-            Nama anda : <input type="text" name="nama" size="25" maxlength="50">
-            Email address : <input type="text" name="email" size="25"
-            maxlength="50">
-            Komentar : <textarea name="komentar" cols="40" rows="5"></textarea>
-            <input type="text" value="kirim">
+            Nama anda            : <input type="text" name="nama" size="25" maxlength="50">
+            Email address        : <input type="email" name="email" size="40" maxlength="50">
+            Komentar             : <textarea name="komentar" id="" cols="40" rows="5"></textarea>
+            <input type="submit" value="kirim">
             <input type="reset" value="ulangi">
-
         </pre>
     </form>
 </body>
